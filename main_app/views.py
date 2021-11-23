a209b2655de7f003603efbfd54d79d1e42f0456c
bnagle22 <naglebenjamin@gmail.com>
2021-11-23 12:42:34 -0500

    create home page

--- main_app/views.py
+++ main_app/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Recipes </h1>')
+  return render(request, 'about.html')
+
+def recipes_index(request):
+  return render(request, 'recipes/index.html', { 'recipes': recipes })
+
+def about(request):
+  return render(request, 'about.html')
+
+class Recipe:
+  def __init__(self, name, ingredients, steps):
+    self.name = name
+    self.ingredients = ingredients
+    self.steps = steps
+
+recipes = [
+  Recipe('Eggs', ['eggs', 'oil'], ['add oil to pan', 'cook eggs'])
+]
